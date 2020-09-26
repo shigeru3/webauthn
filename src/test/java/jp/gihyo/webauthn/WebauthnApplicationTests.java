320176921f54ae75f879ea5e55051b868e53b3b7
shigeru <hironoki3@gmail.com>
2020-09-26 20:03:26 +0900

    initial commit

---
+++ src/test/java/jp/gihyo/webauthn/WebauthnApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gihyo.webauthn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebauthnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
